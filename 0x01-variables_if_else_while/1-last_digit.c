92b56e7f4178a03b677874b5792b14ebea571bcb
William Njoya <williamnjoya@gmail.com>
2022-09-09 15:17:28 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 int Last_Dig (int n)
 {
-	return (n % 10);
+return (n % 10);
 }
-if (Last_Dig > 5)                                                                                                                       {                                                                                                                                       
-printf("Last digit of %d is %d and is greater than 5\n",n,Last_Dig);                                                                    }                                                                                                                                       else if (n == 0 )
-{           
+if (Last_Dig > 5)   
+{                                                                                                                                   printf("Last digit of %d is %d and is greater than 5\n",n,Last_Dig);                                                                  }
+else if (n == 0)
 printf("Last digit of %d is %d and is 0\n",n,Last_Dig);
-}                                                                                                                                       
-else if(Last_Dig < 6 and Last_dig !=0 )                                                                                                 {                                                                                                                              
-printf("Last digit of %d is %d and is less than 6 and not 0", n,Last_Dig);                                                              
+}                                                                                                                                     else if(Last_Dig < 6 and Last_dig !=0 )                                                                                               
+}
+printf("Last digit of %d is %d and is less than 6 and not 0", n,Last_Dig);    
 }
 else
 {
