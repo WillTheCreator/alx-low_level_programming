c05679fa7e950e4955b71875da96e2b5cd741bfa
William Njoya <williamnjoya@gmail.com>
2022-09-09 08:41:07 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-#include <string.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
+
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return (0);
+		
+	puts("\"Programming is like building a multilingual puzzle")   
+       	return (0);
+
 }
