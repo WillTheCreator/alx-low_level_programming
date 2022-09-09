2591aadc1c1431d692a8dc238888632764a94b55
William Njoya <williamnjoya@gmail.com>
2022-09-09 09:37:53 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+#include<conio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void main()
+
+{
+
+	clrscr();
+
+	printf(“TypettttSize (bytes)”);
+
+	printf(“nCharacterttt    %d”,sizeof(char));
+
+	printf(“nIntegertttt    %d”,sizeof(int));
+
+	printf(“nLong intttt    %d”,sizeof(long int));
+
+	printf(“nFloattttt    %d”,sizeof(float));
+
+	printf(“nDoubletttt    %d”,sizeof(double));
+
+	printf(“nLong doublettt    %d”,sizeof(long double));
+
+	getch();
+
+}
