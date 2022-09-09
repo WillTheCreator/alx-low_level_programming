2c3ee63d67216d838baf71aa3327512b3fd04cca
William Njoya <williamnjoya@gmail.com>
2022-09-09 13:55:10 -0700

    1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ ** main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if
+return (0);
+
+}
