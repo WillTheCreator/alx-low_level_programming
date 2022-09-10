66ec7f3889daa34cdaff909fc9616ac8a3798408
William Njoya <williamnjoya@gmail.com>
2022-09-09 17:14:28 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 char alpha_s;
 char alpha_b;
 char new = '\n';
-for (alpha_S = 'a' ; alpha_s <= 'z' ; ++alpha_s)
+for (alpha_s = 'a' ; alpha_s <= 'z' ; ++alpha_s)
 {
 putchar(alpha_s);
 for (alpha_b = 'A' ; alpha_b <= 'Z' ; ++alpha_b)
