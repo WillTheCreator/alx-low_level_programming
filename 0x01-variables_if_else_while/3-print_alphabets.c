327f60f94cde53b27829f921a60b3dcc9677a251
William Njoya <williamnjoya@gmail.com>
2022-09-09 17:08:56 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+char alpha_s;
+char alpha_b;
+char new = '\n';
+for (alpha_S = 'a' ; alpha_s <= 'z' ; ++alpha_s)
+{
+putchar(alpha_s);
+for (alpha_b = 'A' ; alpha_b <= 'Z' ; ++alpha_b)
+{
+putchar(alpha_b)
+}
+}
+putchar(new);
+return (0);
+}
