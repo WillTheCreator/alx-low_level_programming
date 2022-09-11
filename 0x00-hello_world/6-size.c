9fa7ccdaf6c9845086210df7860f82215d6fbf3c
William Njoya <williamnjoya@gmail.com>
2022-09-11 00:37:42 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,28 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-
-
 void main()
-
 {
-
 	clrscr();
-
 	printf(“TypettttSize (bytes)”);
-
 	printf(“nCharacterttt    %d”,sizeof(char));
-
 	printf(“nIntegertttt    %d”,sizeof(int));
-
 	printf(“nLong intttt    %d”,sizeof(long int));
-
 	printf(“nFloattttt    %d”,sizeof(float));
-
 	printf(“nDoubletttt    %d”,sizeof(double));
-
 	printf(“nLong doublettt    %d”,sizeof(long double));
-
 	getch();
-
 }
