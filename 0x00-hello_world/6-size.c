d8b6529d749234ed15610e4d8bff25396c131526
William Njoya <williamnjoya@gmail.com>
2022-09-11 02:53:04 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-void main()
+int main(void)
 {
-	clrscr();
-	printf(“TypettttSize (bytes)”);
-	printf(“nCharacterttt    %d”,sizeof(char));
-	printf(“nIntegertttt    %d”,sizeof(int));
-	printf(“nLong intttt    %d”,sizeof(long int));
-	printf(“nFloattttt    %d”,sizeof(float));
-	printf(“nDoubletttt    %d”,sizeof(double));
-	printf(“nLong doublettt    %d”,sizeof(long double));
-	getch();
+	printf("Size of a char: 1 byte(s)",sizeof(char));
+	printf
+	
 }
