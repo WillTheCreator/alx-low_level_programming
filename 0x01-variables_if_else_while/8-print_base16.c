ab2e27d451e132943eda5d947d2597c9d7aaaa22
William Njoya <williamnjoya@gmail.com>
2022-09-12 10:13:22 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+	char letter;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar((digit % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; ketter++)
+		putchar(letter);
+
+	putchaar('\n');
+
+	return (0);
+}
