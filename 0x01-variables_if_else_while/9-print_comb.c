b92084809e811d2f51cb5e93f01c714bad1815d5
William Njoya <williamnjoya@gmail.com>
2022-09-11 17:25:22 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void);
+{
+	int a = '0';
+	for (a; a < '9' ;a++)
+	{
+		putchar(a);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return(0);
+}
