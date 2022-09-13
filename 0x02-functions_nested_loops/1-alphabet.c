a546e6ba00a90b8426c13bec87204bcf00d7ed79
William Njoya <williamnjoya@gmail.com>
2022-09-13 02:28:26 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Level
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return(0);
+}
+
+
