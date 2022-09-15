e580a766d479f0c342ff0d3d8763133128895cc3
William Njoya <williamnjoya@gmail.com>
2022-09-14 20:03:30 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char c;
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		i++;
-		_putchar('\n');
-	}
+int i = 0;
+char c;
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+i++;
+_putchar('\n');
+}
 }
