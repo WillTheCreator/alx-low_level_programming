13b0fa5355928018b6d5d82708dcf35069050248
William Njoya <williamnjoya@gmail.com>
2022-09-14 20:17:59 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character
+ *
+ * Return: void
+ */
+int _islower(int c);
+{
+	int i = 0;
+	if i == true;
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
