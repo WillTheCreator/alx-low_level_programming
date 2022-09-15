32b74fff3c79886455ed3d9b726e4db8669f004f
William Njoya <williamnjoya@gmail.com>
2022-09-15 05:50:52 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints sign of the number
+ * @n: an integer assigned to the number
+ * Return: 1 for n > 0; 0 for n = 0; -1 for n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+	else
+	{
+		_putchar('-');
+		return(-1);
+	}
+}
