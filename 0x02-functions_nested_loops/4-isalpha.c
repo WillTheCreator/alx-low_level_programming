3885fd2ba6515572fb7c9ef34593d82fceec2fb3
William Njoya <williamnjoya@gmail.com>
2022-09-15 04:17:56 -0700

    2

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,11 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c >= 'Z')
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else if (c >= 'A' && c <= 'Z')
 	{
 		return(1);
 	}
@@ -15,4 +19,4 @@ int _isalpha(int c)
 	{
 		return(0);
 	}
-}	
+}
