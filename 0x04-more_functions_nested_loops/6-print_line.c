9a2c2f57304b2def453f870ac54bc24cfc07e784
William Njoya <williamnjoya@gmail.com>
2022-09-16 13:25:17 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_line - a fn that draws a straight line
  * @n: parameter
  * Return: void
  */
+
 void print_line(int n)
 {
 	while (n-- > 0)
