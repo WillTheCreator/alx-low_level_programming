9bb7bf9e74eb3825598bdb558842acff8448c1a4
William Njoya <williamnjoya@gmail.com>
2022-09-15 17:37:20 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @c: integer variable storing a digit
+ * @d: integer variable storing a digit
+ * Return: Always 0
+ */
+int add(int c, int d)
+{
+	int addition = int c + int d;
+	return(0);
+}
+
+
