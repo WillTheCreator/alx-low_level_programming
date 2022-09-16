4eeb1af34a76424c2b6995c21a47a89d26b45922
William Njoya <williamnjoya@gmail.com>
2022-09-15 17:40:58 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,12 +4,11 @@
  * add - function that adds two integers and returns the result
  * @c: integer variable storing a digit
  * @d: integer variable storing a digit
- * Return: Always 0
+ * Return: addition of the two numbers
  */
 int add(int c, int d)
 {
-	int addition = int c + int d;
-	return(0);
+	return (c + d);
 }
 
 
