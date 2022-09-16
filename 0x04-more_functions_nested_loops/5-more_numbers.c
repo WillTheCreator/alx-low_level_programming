ef21392addc941f11b38b87a110c9e3ebe0e2a3e
William Njoya <williamnjoya@gmail.com>
2022-09-16 12:52:51 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,18 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int x;
-	for (x = 0; x < 10; x++)
-	{
-		for ( i = 0; i <= 14; i++)
-		{
-			if (i > 9)
-			{
-				_putchar((i / 10) + '0');
-			}
-			_putchar((i % 10) + '0');
-		}
-		_putchar('\n');
-	}
+int i;
+int x;
+for (x = 0; x < 10; x++)
+{
+for ( i = 0; i <= 14; i++)
+{
+if (i > 9)
+{
+_putchar((i / 10) + '0');
+}
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
+}
 }
