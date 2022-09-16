c0a50c70d07d2e8de2f658d95d260800508b52fd
William Njoya <williamnjoya@gmail.com>
2022-09-16 13:22:41 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - a fn that draws a straight line
+ * @n: parameter
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
