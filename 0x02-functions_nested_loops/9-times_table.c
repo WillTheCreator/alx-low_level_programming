51cb9a000de5c2fece9c7409c5a0659c617be892
William Njoya <williamnjoya@gmail.com>
2022-09-15 17:03:50 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,13 @@ void times_table(void)
 {
 	int i = 0;
 	int x = 0;
+	int prod;
 	while (i <= 9)
 	{
 		while (x <= 9)
 		{
 			prod = i * x
-			_putchar('prod');
+			_putchar(prod);
 			x++;
 			_putchar('\n');
 		}
