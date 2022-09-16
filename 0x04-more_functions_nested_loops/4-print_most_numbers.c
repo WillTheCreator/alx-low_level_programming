fafa8b0e3e6d7689e598fcb88634cb31d2a702e2
William Njoya <williamnjoya@gmail.com>
2022-09-16 08:20:48 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 38; i < 48; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if ((i == 40) || (i == 42))
+		if ((i == 50) || (i == 52))
 		{
 			continue;
 		}
