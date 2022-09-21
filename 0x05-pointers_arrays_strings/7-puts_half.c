e3868d921a987a7e52e33be7c355496380fe9fc9
William Njoya <williamnjoya@gmail.com>
2022-09-20 19:26:09 -0700

    8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string, followed by a new line
+ * @str: parameter
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (*(str
+
