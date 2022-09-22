4d0058e3404e856ab26a75556625ea7a0878e9d5
William Njoya <williamnjoya@gmail.com>
2022-09-22 10:20:55 -0700

    main.c created

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main- calls other functions
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	vhar *str = "I am a string";
+
+	stringManipulation(str);
+
+	return (0);
+
+}
