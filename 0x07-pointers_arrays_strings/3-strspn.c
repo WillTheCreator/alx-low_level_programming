a95a4540f6a226867ad2fa9062f5fcdcd0aa4dff
William Njoya <williamnjoya@gmail.com>
2022-09-27 19:43:52 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,25 +10,17 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	int c = 0;
+	unsigned int i, j;
 
-	for (i = 0; s[i] = '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		if (s != 32)
+		for (j = 0; accept[j]; j++)
 		{
-			for (j = 0; accept[j] = '\0'; j++)
-			{
-				if (s[i] == accept[j])
-				{
-					c++;
-				}
-			}
+			if (s[i] == accept[j])
+				break;
 		}
-		else
-		{
-			return (c);
-		}
+		if (!accept[j])
+			break;
 	}
-	return (c);
+	return (i);
 }
