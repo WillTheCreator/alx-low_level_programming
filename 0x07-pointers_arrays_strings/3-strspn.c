4433ecfb2d86235464a1e4699bbe960258fae04d
William Njoya <williamnjoya@gmail.com>
2022-09-27 19:03:45 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @s: segment to return bytes from
  * @accept: the bytes to include
  * Return: the number of bytes in the initial segment of @s which consist only
- * of bytes from @accept 
+ * of bytes from @accept
  */
 
 unsigned int _strspn(char *s, char *accept)
